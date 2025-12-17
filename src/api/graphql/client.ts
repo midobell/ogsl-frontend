2edb05ddc4e5e9b91febb5d10093250f2f9a47e3
midobell <belm0284@uqar.ca>
2025-12-17 10:02:30 -0500

    Update GraphQL client URI to use environment variable

--- src/api/graphql/client.ts
+++ src/api/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:8000/graphql/",
+  uri: `${import.meta.env.VITE_API_BASE_URL}/graphql/`,
 });
 
 export const graphQLClient = new ApolloClient({
